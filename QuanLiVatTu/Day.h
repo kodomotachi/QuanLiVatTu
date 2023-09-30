c3c019828b58a8289618466d1d231bc8ffdf72b3
kodomotachi <kodomotachi2k4@gmail.com>
2023-09-30 14:39:29 +0700

    just update

--- QuanLiVatTu/Day.h
+++ QuanLiVatTu/Day.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "mylib.h"
 #include <ctime>
 
 const int DAY_MONTH[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -12,3 +13,15 @@ struct NgayLapHD {
 };
 */
 
+struct Date
+{
+    char day[3];
+    char months[3];
+    char year[5];
+};
+
+char* getDateString(Date& Date)
+{
+    char s[50];
+
+}
---
+++ QuanLiVatTu/UI.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "graphics.h"
+#pragma comment(lib, "graphics.lib")
+
+class UI
+{
+	protected:
+		int left, top;
+		int right, bottom;
+		int backgroundColor, onSelectBakgroundColor
+};
