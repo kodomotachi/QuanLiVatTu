03e7340a9aa7e17e6b33e5c25be10e5011f91604
kodomotachi <kodomotachi2k4@gmail.com>
2024-01-01 17:50:39 +0700

    adjust something in staff.h

--- QuanLiVatTu/staff.h
+++ QuanLiVatTu/staff.h
@@ -67,6 +67,8 @@ void staff_writeData()
 		outtextxy(textWidth, textHeight, s[i]);
 	}
 
+	char title[5][30] = { "NUMBER", "ID", "FIRST NAME", "LAST NAME", "SEX" };
+
 	int cnt = 0, index_staff = 0;
 
 	ifstream input("staff.txt"); // open file staff.txt
@@ -76,8 +78,6 @@ void staff_writeData()
 		std::cout << "File nhu con cac, viet lai file moi cho bo may";
 		return void();
 	}
-
-	input.ignore();
 
 	// 15 40 10
 
@@ -109,12 +109,20 @@ void staff_writeData()
 		++cnt;
 	}
 
-	for (int i = 0; i < index_staff; i++)
+	for (int i = 0; i < 10; i++)
 	{
-		cout << "Ma nhan vien: " << arr[i].MANV << "\n";
-		cout << "Ho: " << arr[i].HO << "\n";
-		cout << "Ten: " << arr[i].TEN << "\n";
-		cout << "Phai: " << (arr[i].PHAI == 0 ? "Nam" : "Nu") << "\n\n";
+		outtextxy((438 + 225 - textwidth(arr[i].MANV)) / 2, (200 + i * 36 + 200 + (i + 1) * 36 - textheight(arr[i].MANV)) / 2, arr[i].MANV);
+		outtextxy((850 + 438 - textwidth(arr[i].HO)) / 2, (200 + i * 36 + 200 + (i + 1) * 36 - textheight(arr[i].HO)) / 2, arr[i].HO);
+		outtextxy((1063 + 850 - textwidth(arr[i].TEN)) / 2, (200 + i * 36 + 200 + (i + 1) * 36 - textheight(arr[i].TEN)) / 2, arr[i].TEN);
+		
+		string check;
+		
+		if (arr[i].PHAI)
+			check = "Nu";
+		else
+			check = "Nam";
+		char *tmp = &check[0];
+		outtextxy((1155 + 1063 - textwidth(tmp)) / 2, (200 + i * 36 + 200 + (i + 1) * 36 - textheight(tmp)) / 2, tmp);
 	}
 
 	input.close();
